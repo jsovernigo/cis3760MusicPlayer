0a06ad8780adbb568a27bab760ebb906e5c6ee2f
Avital <aostromi@uoguelph.ca>
2018-02-13 12:02:16 -0500

    updated searchbar with working text changed listener

--- src/com/smithdtyler/prettygoodmusicplayer/AbstractMusicList.java
+++ src/com/smithdtyler/prettygoodmusicplayer/AbstractMusicList.java
@@ -27,6 +27,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 /**
@@ -69,6 +70,23 @@ public abstract class AbstractMusicList extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.abstract_music_list_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_search);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Call a search method
+                return false;
+            }
+        });
+
         return true;
     }
 
