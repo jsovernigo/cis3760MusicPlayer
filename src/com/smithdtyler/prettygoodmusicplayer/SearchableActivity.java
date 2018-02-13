4deb27cdee84922cab5ae6674bf718f6910c852a
Avital <aostromi@uoguelph.ca>
2018-02-12 19:21:56 -0500

    adding searchbar files

---
+++ src/com/smithdtyler/prettygoodmusicplayer/SearchableActivity.java
@@ -0,0 +1,25 @@
+package com.smithdtyler.prettygoodmusicplayer;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SearchableActivity extends ListActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search);
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            // do the stuff
+        }
+
+    }
+
+}
