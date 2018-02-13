a59e07c6beda68c9647e9a18ff0afa979ff25dd8
Dragmire <liamdragmire1@gmail.com>
2018-02-13 04:10:46 -0500

    Added a Song class

---
+++ src/com/smithdtyler/prettygoodmusicplayer/Song.java
@@ -0,0 +1,27 @@
+package com.smithdtyler.prettygoodmusicplayer;
+
+/**
+ * Created by Liam on 2018-02-13.
+ */
+
+public class Song {
+    private String name;
+    private String artist;
+    private String album;
+    private String file;
+
+
+    public Song(String songName,String artistName,String albumName,String fileName){
+        name = songName;
+        artist = artistName;
+        album = albumName;
+        file = fileName;
+    }
+
+    public String getSongName(){return name;}
+    public String getArtist(){return artist;}
+    public String getAlbum(){return album;}
+    public String getFileName(){return file;}
+
+
+}
