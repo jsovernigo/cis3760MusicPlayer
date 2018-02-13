60b24a7c33ebf9977d9c879b8e07d915c7606bb2
Liam Ewasko <lewasko@uoguelph.ca>
2018-02-13 06:46:27 -0500

    Added comparators to the Song class and added a prototype for a storage class for the Song class called Songs

--- src/com/smithdtyler/prettygoodmusicplayer/Song.java
+++ src/com/smithdtyler/prettygoodmusicplayer/Song.java
@@ -5,6 +5,8 @@ package com.smithdtyler.prettygoodmusicplayer;
  * Pretty basic song object to store data
  */
 
+import java.util.*;
+
 public class Song {
     private String name;
     private String artist;
@@ -24,5 +26,26 @@ public class Song {
     public String getAlbum(){return album;}
     public String getFileName(){return file;}
 
+    public static class SongNameComparator implements Comparator<Song> {
+        @Override
+        public int compare(final Song s1,final Song s2){
+            return s1.getSongName().compareTo(s2.getSongName());
+        }
+    }
+
+    public static class ArtistNameComparator implements Comparator<Song>{
+        @Override
+        public int compare(final Song s1,final Song s2){
+            return s1.getArtist().compareTo(s2.getArtist());
+        }
+    }
+
+    public static class AlbumNameComparator implements Comparator<Song>{
+        @Override
+        public int compare(final Song s1,final Song s2){
+            return s1.getAlbum().compareTo(s2.getAlbum());
+        }
+    }
 
 }
+
