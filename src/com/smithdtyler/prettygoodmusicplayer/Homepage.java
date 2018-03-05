bdbd159e40980e93b4e05d74fdb58b21b22f5d0b
Gurditt Rai <raig@DESKTOP-GURDITT.localdomain>
2018-03-05 16:48:00 -0500

    string descriptions

--- src/com/smithdtyler/prettygoodmusicplayer/Homepage.java
+++ src/com/smithdtyler/prettygoodmusicplayer/Homepage.java
@@ -5,10 +5,10 @@ import android.content.Intent;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Homepage extends Activity {
+    private Object currentTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +16,13 @@ public class Homepage extends Activity {
         setContentView(R.layout.activity_homepage);
 
         //Buttons
-        ImageButton btn_home = (ImageButton) findViewById(R.id.btn_home);
+        ImageButton btn_browse = (ImageButton) findViewById(R.id.btn_browse);
         ImageButton btn_playlists = (ImageButton) findViewById(R.id.btn_playlists);
         ImageButton btn_search = (ImageButton) findViewById(R.id.btn_search);
         ImageButton btn_settings = (ImageButton) findViewById(R.id.btn_settings);
 
         //Move to Home
-        btn_home.setOnClickListener(new View.OnClickListener(){
+        btn_browse.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Homepage.this, ArtistList.class);
