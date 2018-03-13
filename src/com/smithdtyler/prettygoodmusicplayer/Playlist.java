5d0800105ec2372056b7e8a62abdc0ed7b3a5e54
Gurditt Rai <raig@DESKTOP-GURDITT.localdomain>
2018-03-13 14:07:30 -0400

    add works, temp'ly replaced custom dialog

--- src/com/smithdtyler/prettygoodmusicplayer/Playlist.java
+++ src/com/smithdtyler/prettygoodmusicplayer/Playlist.java
@@ -3,20 +3,21 @@ package com.smithdtyler.prettygoodmusicplayer;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class Playlist extends Activity {
     private ArrayList<String> playlist;
+    private ListView lv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         playlist = new ArrayList<>();
@@ -24,10 +25,21 @@ public class Playlist extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
 
+        //List
+        lv = (ListView) findViewById(R.id.playlistListView);
+
         //Buttons
         ImageButton btn_add = (ImageButton) findViewById(R.id.btn_pladd);
         //ImageButton btn_edit = (ImageButton) findViewById(R.id.btn_pledit);
         //ImageButton btn_delete = (ImageButton) findViewById(R.id.btn_pldelete);
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_list_item_1, playlist);
+
+        lv.setAdapter(arrayAdapter);
 
 
         //Add Item to Playlist
@@ -38,44 +50,29 @@ public class Playlist extends Activity {
             @Override
             public void onClick(View v) {
                 //Dialog and Layout
-                View mView = getLayoutInflater().inflate(R.layout.dialog_playlist, null);
                 AlertDialog.Builder builder = new AlertDialog.Builder(Playlist.this);
-                builder.setView(mView);
-
-                //Buttons on Dialog
-                Button ok = (Button) findViewById(R.id.btn_dialog_ok);
-                Button cancel = (Button) findViewById(R.id.btn_dialog_cancel);
+                builder.setTitle("playlist name");
 
                 //Input Field on Dialog
-                final EditText input = (EditText) findViewById(R.id.dialog_plname);
+                final EditText input = new EditText(Playlist.this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_AUTO_CORRECT);
+                builder.setView(input);
 
-                builder.setCancelable(true).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         playlistname = input.getText().toString();
                         playlist.add(playlistname);
                     }
                 });
-
-                /*
-                //Ok Button
-                ok.setOnClickListener(new View.OnClickListener() {
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onClick(View v) {
-                        playlistname = input.getText().toString();
-                        playlist.add(playlistname);
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
                     }
                 });
-                //Cancel Button
-                cancel.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        //onCreateDialog().cancel();
-                    }
-                });*/
 
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                builder.show();
             }
         });
     }
