b74ccafd6f65903995a81ea1cda8c7cad140d484
Gurditt Rai <raig@DESKTOP-GURDITT.localdomain>
2018-03-15 15:58:36 -0400

    saves the items added

--- src/com/smithdtyler/prettygoodmusicplayer/Playlist.java
+++ src/com/smithdtyler/prettygoodmusicplayer/Playlist.java
@@ -2,8 +2,11 @@ package com.smithdtyler.prettygoodmusicplayer;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -21,6 +24,7 @@ public class Playlist extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         playlist = new ArrayList<>();
+        loadPlaylist(getApplicationContext());
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
@@ -61,8 +65,12 @@ public class Playlist extends Activity {
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        //Add item to Playlist
                         playlistname = input.getText().toString();
                         playlist.add(playlistname);
+
+                        //Save Playlist
+                        savePlaylist();
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -75,5 +83,28 @@ public class Playlist extends Activity {
                 builder.show();
             }
         });
+    }
+
+    private void savePlaylist() {
+        SharedPreferences savesharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = savesharedPref.edit();
+        editor.putInt("playlist_size", playlist.size());
+
+        for (int i = 0; i < playlist.size(); i = i + 1) {
+            editor.remove("playlist" + i);
+            editor.putString("playlist" + i, playlist.get(i));
+        }
+
+        editor.commit();
+    }
+
+    private void loadPlaylist(Context context) {
+        SharedPreferences loadsharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        int size = loadsharedPref.getInt("playlist_size", 0);
+        playlist.clear();
+
+        for (int i = 0; i < size; i = i + 1) {
+            playlist.add(loadsharedPref.getString("playlist" + i,null));
+        }
     }
 }
